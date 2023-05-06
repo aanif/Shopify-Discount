761bcf34d123276e29ec9b046bba80c0347531bd
Abdul Wahab Tariq <wahabtariq53@gmail.com>
2023-05-07 03:33:16 +0500

    removed FE-2

--- web/frontend-2/src/app/services/shopifyService.ts
+++
@@ -1,7 +0,0 @@
-export async function getPriceRules() {
-    const response = await fetch(`http://127.0.0.1:8000/api/priceRule`, {
-        method: 'GET',
-        headers: {'Content-Type': 'application/json'},
-      })
-    return await response.json();
-}
