f011202cd6954dd37508eebca8091f0c69f2964a
Muhammad406 <stackingali@gmail.com>
2023-05-08 06:11:39 +0500

    naming, modal and radio buttons

--- web/frontend-2/src/App.js
+++ web/frontend-2/src/App.js
@@ -1,15 +1,16 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
+import FullScreenDialog from './components/modal';
 
 function App() {
-    const [allPrices, setAllPrices] = useState([]);
+    const [offers, setOffers] = useState([]);
 
     useEffect(() => {
-        getAllPricess();
+        getAllOffers();
     }, [])
 
-    const getAllPricess = async () => {
+    const getAllOffers = async () => {
         const res = await axios.get('http://127.0.0.1:8000/api/priceRule');
         res.data.price_rules.forEach(async(item)=>{
             const startDateTime = new Date(item.starts_at);
@@ -19,24 +20,24 @@ function App() {
             item.starts_at=startLocalDateTimeString
             item.ends_at=endLocalDateTimeString
         })
-        setAllPrices(res.data.price_rules);
+        setOffers(res.data.price_rules);
     }
 
   return (
     <div className="App container">
-        {allPrices.length > 0 && <table border="1">
+        {offers.length > 0 && <table border="1">
             <tbody>
                 <tr>
                     <th>Title</th>
                     <th>starts at</th>
                     <th>ends at</th>
                 </tr>
-                {allPrices.map((item, index) => (
+                {offers.map((item, index) => (
                     <tr key={index}>
                         <td>{item.title}</td>
                         <td>{item.starts_at}</td>
                         <td>{item.ends_at}</td>
-                        <td className="button hover" onClick={()=>console.log("play")}>check offer</td>
+                        <td className=""><FullScreenDialog offer={item}/></td>
                     </tr>
                 ))}
             </tbody>
---
+++ web/frontend-2/src/components/modal.js
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import ListItemText from '@mui/material/ListItemText';
+import ListItem from '@mui/material/ListItem';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import CloseIcon from '@mui/icons-material/Close';
+import Slide from '@mui/material/Slide';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+const RadioButtonsGroup=()=> {
+  return (
+    <FormControl>
+      <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-radio-buttons-group-label"
+        defaultValue="Product_id_A"
+        name="radio-buttons-group"
+      >
+        <FormControlLabel value="Product_id_A" control={<Radio />} label="Product A" />
+        <FormControlLabel value="Product_id_B" control={<Radio />} label="Product B" />
+      </RadioGroup>
+    </FormControl>
+  );
+}
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function FullScreenDialog(offer) {
+  const [open, setOpen] = React.useState(false);
+
+  console.log("the offer is: ", offer)
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button color='primary' onClick={handleClickOpen}>
+        check offer
+      </Button>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar sx={{ position: 'relative' }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Offer A
+            </Typography>
+            <Button autoFocus color="inherit">
+              edit
+            </Button>
+            {/* <Button autoFocus color="inherit" onClick={handleClose}>
+              save
+            </Button> */}
+          </Toolbar>
+        </AppBar>
+        <List>
+          <ListItem button>
+            <ListItemText primary="product A" secondary="Titania" />
+            <ListItemText primary="$$$" />
+          </ListItem>
+          <Divider />
+          <ListItem button>
+            <ListItemText
+              primary="product B"
+              secondary="Tethys"
+            />
+            <ListItemText primary="$$$" />
+          </ListItem>
+        </List>
+        <div>
+        {RadioButtonsGroup()}
+        </div>
+      </Dialog>
+    </div>
+  );
+}
