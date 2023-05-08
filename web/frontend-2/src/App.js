15c9760f7ddc1261b00ab0fe8946aba491a1f6ef
Muhammad406 <stackingali@gmail.com>
2023-05-09 01:38:51 +0500

    create offer modal

--- web/frontend-2/src/App.js
+++ web/frontend-2/src/App.js
@@ -1,7 +1,8 @@
 import {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
-import FullScreenDialog from './components/modal';
+import FullScreenDialog from './components/modal.js';
+import AddOffer from './components/addModal.js';
 
 function App() {
     const [offers, setOffers] = useState([]);
@@ -32,24 +33,29 @@ function App() {
 
   return (
     <div className="App container">
-        {offers.length > 0 && <table border="1">
-            <tbody>
-                <tr>
-                    <th>Title</th>
-                    <th>starts at</th>
-                    <th>ends at</th>
-                </tr>
-                {offers.map((item, index) => (
-                    <tr key={index}>
-                        <td>{item.title}</td>
-                        <td>{item.starts_at}</td>
-                        <td>{item.ends_at}</td>
-                        <td className=""><FullScreenDialog offer={item}/></td>
-                        <td onClick={()=>deleteOffer(item)}>Delete</td>
+        {offers.length > 0 && 
+        <div>
+            <table border="1">
+                <tbody>
+                    <tr>
+                        <th>Title</th>
+                        <th>starts at</th>
+                        <th>ends at</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>}
+                    {offers.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.title}</td>
+                            <td>{item.starts_at}</td>
+                            <td>{item.ends_at}</td>
+                            <td className=""><FullScreenDialog offer={item}/></td>
+                            <td onClick={()=>deleteOffer(item)}>Delete</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div><AddOffer /></div>
+        </div>
+        }
     </div>
   );
 }
